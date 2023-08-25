8409bbf70e3f44b6b8b2cc52a51a0ad13516df2b
aaronchewbani <aaronchewbani@gmail.com>
2023-08-25 13:27:11 -0700

    onramp and offramp

---
+++ src/OnOffRampWallet.js
@@ -0,0 +1,140 @@
+import "./styles.css";
+import {
+  convertToValidUserId,
+  useConnector,
+  useCreateFun,
+  configureNewFunStore,
+  MetamaskConnector,
+  Goerli,
+  usePrimaryAuth,
+} from "@fun-xyz/react";
+import { useState } from "react";
+import Modal from "react-modal";
+
+//Step 1: Initialize the FunStore. This action configures your environment based on your ApiKey, chain, and the authentication methods of your choosing. 
+const DEFAULT_FUN_WALLET_CONFIG = {
+  apiKey: "hnHevQR0y394nBprGrvNx4HgoZHUwMet5mXTOBhf",
+  chain: Goerli,
+  gasSponsor: {
+    sponsorAddress: "0xCB5D0b4569A39C217c243a436AC3feEe5dFeb9Ad", //Gasless payments on Goerli. Please switch to another gas sponsor method, or prefund your wallet on mainnet!
+  }
+};
+
+const DEFAULT_CONNECTORS = [
+  MetamaskConnector(),
+];
+
+configureNewFunStore({
+  config: DEFAULT_FUN_WALLET_CONFIG,
+  connectors: DEFAULT_CONNECTORS,
+});
+
+//Step 2: Use the connector button to connect your authentication method, in this case metamask. 
+const ConnectorButton = ({ index }) => {
+  const { active, activate, deactivate, connectorName, connector } = useConnector({ index });
+
+  return (<button
+    onClick={() => {
+      if (active) {
+        deactivate(connector)
+        return
+      }
+      activate(connector)
+    }
+    }>{active ? ("Unconnect") : ("Connect")} {connectorName} </button>)
+}
+
+
+export default function App() {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [url, setUrl] = useState("")
+  const { account: connectorAccount, active } = useConnector({ index: 0, autoConnect: true });
+
+  //Step 3: Use the initializeFunAccount method to create your funWallet object
+  const { account, initializeFunAccount, funWallet } = useCreateFun()
+
+  //Step 4: Use the auth and funWallet to perform actions (ie: swap, transfer, etc.)
+  const [auth] = usePrimaryAuth()
+
+
+  const initializeSingleAuthFunAccount = async () => {
+    // if (!connectorAccount) {
+    //   console.log(await activate())
+    //   return
+    // }
+    initializeFunAccount({
+      users: [{ userId: convertToValidUserId(connectorAccount) }],
+      index: 214
+    }).catch()
+  }
+
+
+  const onRamp = async () => {
+    const url = await funWallet.onRamp()
+    setUrl(url)
+    openModal()
+  }
+
+  const offRamp = async () => {
+    const url = await funWallet.offRamp()
+    setUrl(url)
+    openModal()
+  }
+
+  const openModal = () => {
+    setModalIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalIsOpen(false);
+  };
+
+  return (
+    <div className="App">
+      <h1>Onramp and OffRamp Funds to FunWallet</h1>
+      1. Connect Metamask.
+      <ConnectorButton key={0} index={0} ></ConnectorButton>
+      {
+        active ?
+          <div>
+            Success! Metamask Connected!
+          </div>
+          : <></>
+      }
+      <br></br>
+      <br></br>
+
+      2. Initialize the FunWallet and Auth object.
+      <button onClick={initializeSingleAuthFunAccount}>Initialize FunWallet</button>
+      {account ?
+        <div>
+          Success! FunWallet Address: {account}
+        </div>
+        : <></>
+      }
+      <br></br>
+      <br></br>
+
+      3. Onramp or Offramp funds into FunWallet
+      <button onClick={onRamp} >OnRamp FunWallet</button>
+      <button onClick={offRamp} >OffRamp FunWallet</button>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className="modal"
+        overlayClassName="modal-overlay"
+      >
+        <div id="modalContent" className="modal-content">
+          <iframe
+            id="moonpayIframe"
+            src={url?url:""}
+            title="MoonPay"
+          ></iframe>
+        </div>
+      </Modal>
+      <br></br>
+      <br></br>
+    </div>
+  );
+}
